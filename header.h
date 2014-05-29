afadcd990b5ca1714e16e76813d1d6730370b7c3
Anton Karpus <akarpus2@gmail.com>
2014-05-28 23:28:46 -0400

    Update header.h

--- header.h
+++ header.h
@@ -1,13 +1,8 @@
 #define SERVER_PORT 60002
 #define MAX_STR 100
 
-static volatile int oracle_Signal = 0;
+static volatile int Signal = 0;
 
 // User Functions
 void user2(char*);
 void user1();
-
-/* Signal Handler Functions */
-void yes_Signal(int);
-void no_Signal(int);
-void win_Signal(int);
